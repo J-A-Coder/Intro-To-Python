004af04392f23ce82b9d14c93abeb112b2d36ef7
Joseph Anthony <jo.atwork@yahoo.com>
2025-03-03 14:22:11 -0600

    Added Data Type ch. exercises

---
+++ Data_Types/Exer1.py
@@ -0,0 +1,21 @@
+VALUE
+
+1. 'True'    String    str
+
+2. False     Boolean   bool
+
+3. (1, 2, 3) Tuple     tuple
+
+4. 1.5       Float     float
+
+5. [1, 2, 3] List      list
+
+6. 2         Integer   int
+
+7. range(5)  Range     range
+
+8. {1, 2, 3} Set       set
+
+9. None      NoneType  NoneType
+
+10.{'foo': 'bar'}  Dictionary   dict
