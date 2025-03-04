d1caa583557bb4d08cf9d95ef816e3156af4fbd7
Joseph Anthony <jo.atwork@yahoo.com>
2025-03-04 08:18:25 -0600

    Completed exercises

---
+++ Data_Types/Exer2.py
@@ -0,0 +1,16 @@
+Exercise 2. Create a tuple called names that contians several pet names.
+
+Solution 1.
+
+names = ('Asta', 'Butterscotch', 'Pudding', 'Neptune', 'Darwin').
+
+
+Solution 2.
+
+names = (
+    'Asta',
+    'Betterscotch',
+    'Pudding',
+    'Neptune',
+    'Darwin',
+)
---
+++ Data_Types/Exer3.py
@@ -0,0 +1,17 @@
+Exercise 3. Create a dictionary named pets tha contains a list of pet names and the type of animal.
+
+Solution 1:
+
+pets = {'Asta': 'dog', 'Butterscotch': 'cat', 'Puddng': 'cat', 'Neptune': 'Fish', 'Darwin': 'Lizard'}
+
+
+Solution 2:
+
+pets = {
+    'Asta': 'dog',
+    'Butterscotch': 'cat',
+    'Pudding': 'Cat',
+    'Neptune': 'Fish',
+    'Darwin': 'Lizard',
+}
+
